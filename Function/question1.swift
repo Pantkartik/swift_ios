c58b14fb1706e0e4d96eaaa4aca5ce2835570b98
Kartik Pant <kartikpant.kp69@gmail.com>
2026-04-07 00:27:03 +0530

    feat-function questions

---
+++ Function/question1.swift
@@ -0,0 +1,14 @@
+// check if even or odd 
+
+
+
+func check_evenOdd(n:Int)->Bool{
+    if n%2==0 {
+        return true
+    }
+    else {
+        return false
+    }
+}
+
+print(check_evenOdd(n:10))
---
+++ Function/question2.swift
@@ -0,0 +1,8 @@
+// to square root the number
+
+
+func sqrt(n:Int)->Int{
+    return n*n
+}
+
+print(sqrt(n:3))
