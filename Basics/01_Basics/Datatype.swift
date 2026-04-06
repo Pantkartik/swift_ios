83a689e08b9296a9c06be5b5411323c4f1e0e703
Kartik Pant <kartikpant.kp69@gmail.com>
2026-04-06 23:29:17 +0530

    feat-datatypes swift explicit

---
+++ Basics/01_Basics/Datatype.swift
@@ -0,0 +1,8 @@
+// in swift there are many same datatypes like in other language 
+
+// int , bool ,string , double 
+
+// example 
+
+let name:String="Kartik"
+print(name)
--- Basics/01_Basics/hello.swift
+++ Basics/01_Basics/hello.swift
@@ -8,16 +8,28 @@
 // ....  let is used for a constant value 
 let age=20
 
-print("age")
+print("age") // only prints the age word not the number 
+print(age)
+
 
 
 
 // var 
 // ....  var is used for that datatype which can change it like variable 
-
+var name="Kartik"
+print(name)
+// change in the name 
+name="Pant"
+print(name)
 
 
 
 
 // const 
 // .... const is used for a constant value whose value cannot change in future 
+const G=10
+
+print(G)
+// change in the G
+G=50
+print(G) // not possible variable G is constant
