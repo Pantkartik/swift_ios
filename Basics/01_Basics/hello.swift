bbf338c5a0b70be52bcfd8fe5f3344af3615d42f
Kartik Pant <kartikpant.kp69@gmail.com>
2026-04-06 23:25:13 +0530

    feat-basics swift let,const,var

---
+++ Basics/01_Basics/hello.swift
@@ -0,0 +1,23 @@
+// all the concept in swift are same as the javascript and python mixture 
+
+
+// declaration of the variable is used by let const and var 
+
+
+// let 
+// ....  let is used for a constant value 
+let age=20
+
+print("age")
+
+
+
+// var 
+// ....  var is used for that datatype which can change it like variable 
+
+
+
+
+
+// const 
+// .... const is used for a constant value whose value cannot change in future 
