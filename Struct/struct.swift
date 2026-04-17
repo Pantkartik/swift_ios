ed9a3d846335cde5e98ee6a94c7a73eec00da6c3
Kartik Pant <kartikpant.kp69@gmail.com>
2026-04-17 22:13:45 +0530

    feat: update struct print statements and add Car struct definition

--- Struct/struct.swift
+++ Struct/struct.swift
@@ -7,5 +7,8 @@ var p1=Person(name:"KARTIK")
 var p2=p1
 p2.name="Pant"
 
-print(p1.name)
-print(p2.name)
+print("unchanged the value of p1",p1.name)
+print("p1 template for the person is used for p2",p2.name)
+
+
+struct Car 
